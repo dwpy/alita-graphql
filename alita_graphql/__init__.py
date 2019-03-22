711d715f0a1f1e0698d1d78f504bc8f7f783e2ca
dongwei <dongwei@gstianfu.com>
2019-03-22 16:12:46 +0800

    add alita graphql code

--- alita_graphql/__init__.py
+++ alita_graphql/__init__.py
@@ -6,13 +6,12 @@ __version__ = '0.1.0'
 class GraphQL(object):
     def __init__(self, schema, app=None, **options):
         self.app = app
-        self.init_app(app)
         self.graphql_view = GraphQLView(schema=schema, **options)
+        if app is not None:
+            self.init_app(app)
 
-    def init_app(self, app=None):
-        if app:
-            self.app = app
-        assert self.app, RuntimeError("App object not existed!")
+    def init_app(self, app):
+        self.app = app
 
         @self.app.route('/graphql', methods=['GET', 'POST'])
         async def graphql_index(request):
